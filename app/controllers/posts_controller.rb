220ad93fe87abf177404cf80b1d19d18aaaf889f
Gilbert Torchon <gilbert@noukod.com>
2019-01-22 17:11:45 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,11 @@ class PostsController < ApplicationController
 
   def update
 
-    if @post.valid? 
+    if @post.valid?
       @post.update(post_params)
 
       redirect_to post_path(@post)
-    else 
+    else
       render :edit
     end
 
